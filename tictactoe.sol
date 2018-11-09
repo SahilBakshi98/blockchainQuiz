e4ee0a0ceef00a8d76a8c0a848af03c79c3a7551
aniketshrimal <aniketshrimal98@gmail.com>
2018-11-09 10:44:55 +0530

    Third commit

--- tictactoe.sol
+++ tictactoe.sol
@@ -3,12 +3,18 @@ pragma solidity ^0.4.24;
 contract Tictactoe{
 
 	address public owner;
+	address public challanger;
+	address public opponent;
     
-    uint playercount;
-	
+    
+	modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner is allowed to call this method.");
+        _;
+    }
+
 
 	constructor() public {
-		playercount=0;
+		challanger = msg.sender;	
 	}
 
 	struct Game{
@@ -16,15 +22,38 @@ contract Tictactoe{
 		uint turn;
 		address opponent;
 		mapping(uint => mapping(uint => uint )) board;
+		bool isset;
 	}
 
-	modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner is allowed to call this method.");
-        _;
-    }
+	mapping (address => Game) games;
 
-	function addPlayer(){
 
+
+	
+    function start() 
+    public 
+    payable
+    onlyOwner()
+    {
+    	Game storage g = game[msg.sender];
+
+    	g.balance+=msg.value;
+    	g.isset = true;
+    	g.opponent = 0;
+    } 
+
+	function join() 
+	public
+	payable
+	{
+		Game storage g = games[owner];
+		require(g.isset && g.opponent == 0 && msg.value == g.balance);
+		require(msg.sender != owner);
+		if(g.opponent == 0 )
+		{
+			g.opponent = msg.sender;
+			g.balance+=msg.value;
+		}
 	}  
 
 
